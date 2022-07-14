c9755a3e3da92cabf8b2da4c1fb3134bff81abf9
Róbison Joel Guimarães Simon <64526672+robison-joel@users.noreply.github.com>
2022-07-14 00:15:22 -0300

    Add files via upload

--- fazer/bck_casa_.sh
+++ fazer/bck_casa_.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -x
 
-##################################################
+#########################################################################
 # NOME...: Robison Joel
 # DATA...: 28/04/2022
 # ASSUNTO: Backup do meu PC de casa.
-##################################################
+#########################################################################
 
-de_onde="/home/robisonjoel/*"
+# de_onde="/home/robisonjoel/*"
 
-pra_onde="/mnt/9C8CD0608CD0368C/bckp/"
+# pra_onde="/mnt/9C8CD0608CD0368C/bckp/"
 
-sudo rsync -v -r -p --progress $de_onde $pra_onde
+# rsync -v -r -p --progress $de_onde $pra_onde
+
+sudo scp -rv /media/robisonjoel/HD/bckp/* rjgs@192.168.100.200:/home/rjgs/bckp/
